1809cb9fa7d4c00afbea480e8f6b7858dd6b708d
Alexey-Eremin-lalala <149609252+Alexey-Eremin-lalala@users.noreply.github.com>
2024-05-24 15:50:58 +0300

    2

--- app/api/config.js
+++ app/api/config.js
@@ -1,9 +1,4 @@
-export const BASE_URL = [
-  'https://back.nomoredomainswork.ru/api',
- 'https://front.nomoredomainswork.ru',
- 'http://localhost:3001',
- 'http://localhost:3000'
-];
+export const BASE_URL = ['https://back.nomoredomainswork.ru/api','https://front.nomoredomainswork.ru','http://localhost:3001','http://localhost:3000' ];
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
