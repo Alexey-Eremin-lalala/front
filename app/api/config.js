70025cbf3c3e4652888ec9e75f9b071c222ed8d4
Alexey-Eremin-lalala <149609252+Alexey-Eremin-lalala@users.noreply.github.com>
2024-05-24 15:34:51 +0300

    f

--- app/api/config.js
+++ app/api/config.js
@@ -1,7 +1,7 @@
-export const BASE_URL = ['https://back.nomoredomainswork.ru/api', 'https://front.nomoredomainswork.ru/'];
+export const BASE_URL = ['https://back.nomoredomainswork.ru/api', 'https://front.nomoredomainswork.ru'];
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
   auth: `${BASE_URL}/auth/login`,
   me: `${BASE_URL}/me`
-}; 
+};
